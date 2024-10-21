af675438672980eb23b92f5cb511d90155a85880
Chad McDaniel <93947281+ccmcdaniel@users.noreply.github.com>
2024-10-21 15:00:55 -0500

    added functionality for insertion

--- CSC307F24BSTProblemsH001/BinarySearchTree.h
+++ CSC307F24BSTProblemsH001/BinarySearchTree.h
@@ -1,1 +1,78 @@
 #pragma once
+
+#include "TreeNode.h"
+
+class BinarySearchTree
+{
+private:
+	TreeNode* root = nullptr;
+	int count = 0;
+
+
+public:
+	bool Insert(int data)
+	{
+		if (root == nullptr)
+		{
+			root = new TreeNode;
+			root->data = data;
+			count++;
+			return true;
+		}
+		else
+		{
+			return Insert(data, root);
+		}
+	}
+
+
+private:
+	bool Insert(int data, TreeNode* node)
+	{
+		if (node->data == data)
+			return false;
+		else if (node->data > data)
+		{
+			//Look at the left side.
+
+			//Check to see if a left child exists
+			if (node->leftChild != nullptr)
+			{
+				return Insert(data, node->leftChild);
+			}
+			else
+			{
+				//If left side is empty, insert at the left.
+				TreeNode* new_node = new TreeNode;
+				new_node->data = data;
+				new_node->parent = node;
+				node->leftChild = new_node;
+				count++;
+				return true;
+
+			}
+		}
+		else
+		{
+			//Look at the right side
+
+			//Check to see if a right child exists
+			if (node->rightChild != nullptr)
+			{
+				return Insert(data, node->rightChild);
+			}
+			else
+			{
+				//If left side is empty, insert at the left.
+				TreeNode* new_node = new TreeNode;
+				new_node->data = data;
+				new_node->parent = node;
+				node->rightChild = new_node;
+				count++;
+				return true;
+
+			}
+		}
+	}
+
+};
